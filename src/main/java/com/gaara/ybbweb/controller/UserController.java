4c6ae75ee89d28ed1dbe244899fc35b6ea083f99
guojiawen <yqxdev06@yiqixie.com>
2018-06-01 17:29:06 +0800

    update

--- src/main/java/com/gaara/ybbweb/controller/UserController.java
+++ src/main/java/com/gaara/ybbweb/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -35,5 +37,19 @@ public class UserController {
   @RequestMapping(value = "/list", method = RequestMethod.GET)
   public List<User> list() {
     return userService.getAllUsers();
+  }
+
+  @RequestMapping(value = "/test")
+  public void test1() {
+    String a = "cn=foo-foo_bar1,l=EMEA,dc=foobar,dc=com";
+    Pattern usernameCnPattern = Pattern.compile("(cn=[a-zA-Z0-9_-]+)");
+    Matcher matcher = usernameCnPattern.matcher(a);
+    if (matcher.find()) {
+      System.out.println("gourp0" + matcher.group(0));
+      System.out.println("gourp1" + matcher.group(1));
+      System.out.println("gourp2" + matcher.group(2));
+    }
+
+
   }
 }
